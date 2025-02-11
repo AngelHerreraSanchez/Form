b4c29380c2473f055e8b461f893e6d0cab5ec611
herreang000 <herreang000@warren.k12.in.us>
2025-02-11 12:10:32 -0500

    final

---
+++ script.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    let username = document.getElementById("username").value;
+    if (username === "") {
+        document.getElementById("error").innerText = "Username is required";
+        return false;
+    }
+    return true;
+}
